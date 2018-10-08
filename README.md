c30a27e06bad330168fca0280b624e5e910f510b
UH <554c46@gmail.com>
2018-10-08 12:04:31 +0200

    initial upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# template6
+This template installs a MongoDB server as container.
+
+* Data Container: It uses a specified data volume container that might be created if it not exist yet.
+* Dockerfile: There is no Dockerfile because the MongoDb image run basically out of the box.
+* Credentials: No access restrictions, i.e. no user/pass is set. Make sure nobody have access to the host server
+* smallfiles: The `--smallfiles` flag is applied
+
+## Use cases
+* ELT (Data Lake), cleansed data layer, i.e.
+    * "load" raw data into "cleansed" data layer
+    * start the actual cleaning (create clean key-values)
+    * offer clean key-values for "transform" process to pull
+
+## Commands
+Use the following commands to install, start, or uninstall the images or container.
+
+| command | description |
+|:-------:|:-----------:|
+| `./config uninstall` | Cleanup previous installations |
+| `vi config.conf` | Increment the version |
+| `./config.sh run` | Instantiate a new MongoDB container |
+| `./config.sh start` | Start the Container again |
+
+Requires execution rights for `config.sh`.
+For example, run `chmod u+x config.sh` to call `./config.sh ...`.
+Otherwise call `bash config.sh ...`.
+
+## Links
+* [template6](https://github.com/waalfisk/template6)
---
+++ config.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#cosmetic stuff
+hline=$(printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -)
+
+#check if docker is online
+aw=$(docker stats --no-stream)
+if [ -z "${aw}" ]; then
+    echo "Your docker daemon is offline. Please start your docker daemon first!"
+    echo "Call On MacOS: 'open /Applications/Docker.app'"
+    exit 1
+fi
+
+#load settings
+source config.conf
+
+#get and goto current directory (required to keep relative links working)
+folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd "${folder}"
+
+#abort if no subcommands given
+if [ $# -lt 1 ]; then
+    echo "no subcommands supplied."
+    echo "Usage: ./$0 {run|start|uninstall}"
+    exit 1
+fi
+
+
+#loop over all arguments
+for inarg in "$@"; do 
+#for each $inarg do case-switch
+case "${inarg}" in
+run)
+    echo $hline
+    echo "Create data container '${datacontainer}' if not exist"
+    docker volume create "${datacontainer}"
+    echo "Instantiate container '${containername}' from the '${mongoimage}:${mongoversion}' image now"
+    docker run -it \
+        --name "${containername}" \
+        -v "${datacontainer}:/data" \
+        -p "${containerport}:27017" \
+        -d "${mongoimage}:${mongoversion}" --smallfiles
+    echo "*** Container Instantiation Completed ***"
+    ;;
+start)
+    #no fancy echo/print messages
+    docker start -a ${containername} #attach container
+    ;;
+uninstall)
+    echo $line
+    echo "Delete '${containername}' now"
+    docker stop ${containername}
+    docker rm ${containername}
+    echo "*** Container deleted ***"
+    echo "    Type 'docker rmi ${mongoimage}:${mongoversion}' to delete the base image."
+    echo "    Type 'docker volume rm ${datacontainer}' to delete the data volume."
+    ;;
+*)
+    echo "Usage: ./$0 {run|start|uninstall}"
+    exit 1
+esac #end case-switch
+done #end for-loop
+
+#successfully done
+exit 0
